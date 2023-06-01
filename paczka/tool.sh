619452af3840e5ef0599e643eb50b34e9c1c727f
Mateusz Malinowski <malina314@op.pl>
2023-06-01 19:23:38 +0200

    Add outs for new tests

--- paczka/tool.sh
+++ paczka/tool.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-for file in ./bad/*.jpp
+# for file in ./bad/*.jpp
+# do
+#   ./interpreter -v "$file" 1>"${file%.jpp}.out" 2>"${file%.jpp}.err"
+# done
+
+# for file in ./good/*.jpp
+# do
+#   ./interpreter "$file" 1>"${file%.jpp}.out" 2>"${file%.jpp}.err"
+# done
+
+for file in ./bad/TC-niezadeklarowana-zmienna-0*.jpp
 do
   ./interpreter -v "$file" 1>"${file%.jpp}.out" 2>"${file%.jpp}.err"
 done
 
-for file in ./good/*.jpp
+for file in ./good/09-przesłanianie-i-statyczne-wiązanie-0*.jpp
 do
   ./interpreter "$file" 1>"${file%.jpp}.out" 2>"${file%.jpp}.err"
 done
